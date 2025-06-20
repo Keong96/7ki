70968c9699207b046c391bba93326fca5e942913
Keong96 <chinkeongtan96@gmail.com>
2025-06-20 16:24:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
+  preview: {
+    host: true,
+    port: process.env.PORT ? Number(process.env.PORT) : undefined,
+    allowedHosts: ["sevenki.onrender.com"]
+  }
 })
